d12857a9140072a6ef06ef8b994be4edd557fd6a
Louis Mitas <lmitas@outlook.com>
2024-12-31 19:13:35 -0500

    Testing Mastodon
    Hello world now connects to mastodon and gets the user's recent posts

--- daemon.py
+++ daemon.py
@@ -1,15 +1,42 @@
 from mastodon import Mastodon
 from atproto import Client
 import os
+import html
 
 def hello_world():
+    mastodon = Mastodon(
+        access_token=os.environ["MASTO_TOKEN"],
+        api_base_url=os.environ["MASTO_URL"]
+    )
+
+    my_account = mastodon.me()
+    account_id = my_account["id"]
+
+    recents = mastodon.account_statuses(
+        account_id,
+        limit = 20,
+        exclude_replies=True,
+        exclude_reblogs=True
+    )
+
+    for post in recents:
+        post["plain"] = html.unescape(post["content"])
+        post["plain"] = post["plain"].replace("</p><p>", "\n\n")
+        post["plain"] = post["plain"].replace("<p>", "")
+        post["plain"] = post["plain"].replace("</p>", "")
+
+    return recents
+
+def login_bsky():
     # set up credentials for bsky
     bsky_user = os.environ["BSKY_USER"]
     bsky_pass = os.environ["BSKY_PASS"]
 
     client = Client()
     client.login(bsky_user, bsky_pass)
-    client.send_post(text="If you're reading this, it means I just successfully used the Bluesky API on my very first try.")
+
+def send_to_bsky(bsky_client, post_text):
+    bsky_client.send_post(text=post_text)
 
 if __name__ == "__main__":
     hello_world()
--- environment.sh
+++ environment.sh
@@ -1,2 +1,4 @@
 export BSKY_USER=
 export BSKY_PASS=
+export MASTO_TOKEN=
+export MASTO_URL=
